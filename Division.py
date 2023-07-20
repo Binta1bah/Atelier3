76c63c7b3981df0d0700bbfcb5d7276a934ed55c
Binta1bah <140073219+Binta1bah@users.noreply.github.com>
2023-07-20 16:49:07 +0000

    Division.py: Ajout du script de division en python

---
+++ Division.py
@@ -0,0 +1,9 @@
+def division(a, b):
+    
+# Cette fonction effectue la division de deux nombres (a et b) et retourne le résultat.
+  
+# Test de la fonction
+nombre1 = 10
+nombre2 = 2
+resultat = division(nombre1, nombre2)
+print(f"Le résultat de la division de {nombre1} par {nombre2} est égal à {resultat}.")
