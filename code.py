d651a772b0820db9bf1dff69c3e45ff29b2dbcfa
Binta1bah <fbinta108@gmail.com>
2023-07-20 13:03:48 +0000

    ajout du fichier code.py contenant un code python

---
+++ code.py
@@ -0,0 +1,12 @@
+def somme_deux_nombres(a, b):
+    """
+    Cette fonction prend deux nombres en entrée (a et b) et retourne leur somme.
+    """
+    somme = a + b
+    return somme
+
+# Test de la fonction
+nombre1 = 10
+nombre2 = 5
+resultat = somme_deux_nombres(nombre1, nombre2)
+print(f"La somme de {nombre1} et {nombre2} est égale à {resultat}.")
